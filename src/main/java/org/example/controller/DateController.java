a1b6cadaa623971abe5ae51678882bf6fa725e29
zxcvbnm668813 <2605698492@qq.com>
2025-10-13 12:13:10 +0800

    2025-10-13-1

--- src/main/java/org/example/controller/DateController.java
+++ src/main/java/org/example/controller/DateController.java
@@ -1,4 +1,12 @@
 package org.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class DateController {
+    @GetMapping("/date")
+    public String showDate() {
+        return "date";
+    }
 }
